798d80ebe679f81ef783df5ca863aedad07eadd8
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-15 00:25:57 +0700

    only use on front-end

--- wp-basket.php
+++ wp-basket.php
@@ -9,15 +9,19 @@ Author: nguyenvanduocit
 Author URI: http://nvduoc.senviet.org
 License: GPL2
 */
-
-add_filter( 'script_loader_tag', 'wp_basket_getLoadedScript', 10, 3 );
+add_action('init', 'wp_basket_init');
+function wp_basket_init(){
+	if(!is_admin()){
+		add_filter( 'script_loader_tag', 'wp_basket_getLoadedScript', 10, 3 );
+		add_action( 'wp_head', 'printBasketScript', 1 );
+	}
+}
 function wp_basket_getLoadedScript($tag, $handle, $src){
 	$src = sprintf('{ url: "%1$s", key: "%2$s"}', $src, $handle );
 	$tag = '<script>(function (basket) {basket.require(' . $src . ');})(basket);</script>';
 	return $tag;
 }
 
-add_action( 'wp_head', 'printBasketScript', 1 );
 function printBasketScript(){
 	?>
 	<script src="<?php echo plugins_url('js/basket.js', __FILE__); ?>"></script>
