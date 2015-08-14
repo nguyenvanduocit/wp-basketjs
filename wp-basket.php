203860387cf255b08de505b5cb1b0a7b3956aef4
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-15 00:21:03 +0700

    init

---
+++ wp-basket.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Plugin Name: Wp Basket
+Plugin URI: http://nvduoc.senviet.org/wp_basket
+Description: A plugin to use basket.js to improve js loading on your site
+Version: 1.0
+Author: nguyenvanduocit
+Author URI: http://nvduoc.senviet.org
+License: GPL2
+*/
+
+add_filter( 'script_loader_tag', 'wp_basket_getLoadedScript', 10, 3 );
+function wp_basket_getLoadedScript($tag, $handle, $src){
+	$src = sprintf('{ url: "%1$s", key: "%2$s"}', $src, $handle );
+	$tag = '<script>(function (basket) {basket.require(' . $src . ');})(basket);</script>';
+	return $tag;
+}
+
+add_action( 'wp_head', 'printBasketScript', 1 );
+function printBasketScript(){
+	?>
+	<script src="<?php echo plugins_url('js/basket.js', __FILE__); ?>"></script>
+	<?php
+}
