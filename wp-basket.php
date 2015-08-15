a56d3d56cf1133bbc1eb2f34eb67286ac6a90e26
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-15 07:40:58 +0700

    edit function name for unique

--- wp-basket.php
+++ wp-basket.php
@@ -9,20 +9,20 @@ Author: nguyenvanduocit
 Author URI: http://nvduoc.senviet.org
 License: GPL2
 */
-add_action('init', 'wp_basket_init');
-function wp_basket_init(){
+add_action('init', 'wpb_init');
+function wpb_init(){
 	if(!is_admin()){
-		add_filter( 'script_loader_tag', 'wp_basket_getLoadedScript', 10, 3 );
-		add_action( 'wp_head', 'printBasketScript', 1 );
+		add_filter( 'script_loader_tag', 'wpb_get_loaded_script', 10, 3 );
+		add_action( 'wp_head', 'wpb_print_basket_script', 1 );
 	}
 }
-function wp_basket_getLoadedScript($tag, $handle, $src){
+function wpb_get_loaded_script($tag, $handle, $src){
 	$src = sprintf('{ url: "%1$s", key: "%2$s"}', $src, $handle );
 	$tag = '<script>(function (basket) {basket.require(' . $src . ');})(basket);</script>';
 	return $tag;
 }
 
-function printBasketScript(){
+function wpb_print_basket_script(){
 	?>
 	<script src="<?php echo plugins_url('js/basket.js', __FILE__); ?>"></script>
 	<?php
